0d94ca5afe6f09d53af746a2a0e48afe30e5822f
Miriam Chen <45667250+Miriam410@users.noreply.github.com>
2021-04-10 02:41:21 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicios19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //19. Solicite el ingreso de un número al usuario y presente la parte entera
+            string numero;
+            double SalidaNum;
+            bool Flag = false;
+            do
+            {
+                Console.WriteLine("Ingrese un número");
+                numero = Console.ReadLine();
+                if (!double.TryParse(numero, out SalidaNum))
+                {
+                    Console.WriteLine("Usted debe ingresar un valor numérico");
+                }
+                else
+                {
+                    Flag = true;
+                    Console.WriteLine($"El numero ingresado es {SalidaNum}" +
+                       " y la parte entera es "+ Math.Round(SalidaNum));
+                }
+            } while (Flag==false);
+            Console.WriteLine("Presione una tecla para salir");           
+            Console.ReadKey();   
+        }
+    }
+}
